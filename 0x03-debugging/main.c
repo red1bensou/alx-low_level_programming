37877374320f957e1b04683346de18d4a40e5b06
redouane <redwanerahimaayoub1999@gmail.com>
2023-06-21 10:23:48 -0700

    0-main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -11,4 +13,4 @@ int main(void)
         positive_or_negative(i);
 
         return (0);
-}
+}}
